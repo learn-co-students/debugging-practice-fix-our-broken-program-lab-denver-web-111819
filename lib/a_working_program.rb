acbc6e09e335bc31829de2bf7c8f9881f0ddd9f8
Miriam Grigsby <miriamgrigsby@gmail.com>
2019-11-09 00:46:48 +0000

    Done.

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,4 +1,6 @@
-puts Hello World! # Correct this to "Hello World!"
-
+def printer (string)
+  string = Hello World! # Correct this to "Hello World!"
+  puts string
+end
 # Your entire program should read:
 # puts "Hello World!"
